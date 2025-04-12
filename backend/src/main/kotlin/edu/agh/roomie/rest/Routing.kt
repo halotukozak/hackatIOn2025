94b7072a411c9b0a8db1eff5c89f66160ae8581a
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-04-12 11:23:55 +0200

    feat: add OpenAPI documentation and update routing for Swagger UI

--- backend/src/main/kotlin/edu/agh/roomie/rest/Routing.kt
+++ backend/src/main/kotlin/edu/agh/roomie/rest/Routing.kt
@@ -3,6 +3,7 @@ package edu.agh.roomie.rest
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.plugins.swagger.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -13,8 +14,6 @@ fun Application.configureRouting() {
     }
   }
   routing {
-    get("/") {
-      call.respondText("Hello World!")
-    }
+    swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
   }
 }
