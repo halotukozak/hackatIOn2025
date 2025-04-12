469f807cfb244715b18ac11834d6b5f5cc5992dc
Gawor270 <gaworbernard@gmail.com>
2025-04-12 12:07:15 +0200

    resolve conflict

--- frontend/src/rest/model.ts
+++ frontend/src/rest/model.ts
@@ -34,3 +34,41 @@ export interface Pair {
   first: number;
   second: number;
 }
+
+export enum Departament {
+  WILiGZ = "WILiGZ",
+  WIMiIP = "WIMiIP",
+  WEAIIB = "WEAIIB",
+  WIET = "WIET",
+  WIMiR = "WIMiR",
+  WGGiOS = "WGGiOS",
+  WGGiIS = "WGGiIS",
+  WIMiC = "WIMiC",
+  WO = "WO",
+  WMN = "WMN",
+  WWNiG = "WWNiG",
+  WZ = "WZ",
+  WEiP = "WEiP",
+  WFiIS = "WFiIS",
+  WMS = "WMS",
+  WH = "WH",
+  WI = "WI",
+  WTK = "WTK",
+}
+
+export enum Hobby {
+  basketball = "basketball",
+  football = "football",
+  volleyball = "volleyball",
+  swimming = "swimming",
+  running = "running",
+  cycling = "cycling",
+  hiking = "hiking",
+  climbing = "climbing",
+  skiing = "skiing",
+  music = "music",
+  dance = "dance",
+  cooking = "cooking",
+  painting = "painting",
+  photography = "photography",
+}
