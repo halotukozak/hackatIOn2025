42978d447f71f922945b244cd72c4557e52baa68
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-04-13 20:18:57 +0200

    refactor: update backend URL configuration to use environment variable

--- frontend/src/apis/base.ts
+++ frontend/src/apis/base.ts
@@ -1,4 +1,3 @@
 export function base_url() {
-  return "https://roomie-backend-wnzd.onrender.com";
-  //   return "http://localhost:8080";
+    return import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
 }
